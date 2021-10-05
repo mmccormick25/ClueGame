8cc30df52a96c28e7f30f7ac46efb7ab67399fd4
Zhen Shan Liu <zliu1@mines.edu>
2021-10-05 15:16:34 -0600

    implement the function of findAllTargets

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -9,6 +9,7 @@ public class TestBoard {
 	final static int ROWS = 4;
 	public TestBoardCell[][] testCells = new TestBoardCell[COLS][ROWS];
 	public Set<TestBoardCell> targets = new HashSet<>();
+	HashSet<TestBoardCell> visitedList = new HashSet<TestBoardCell>();
 	public TestBoard() {
 		for (int c = 0; c < COLS; c++) {
 			for (int r = 0; r < ROWS; r++) {
@@ -16,8 +17,21 @@ public class TestBoard {
 			}
 		}
 	}
+	public void findAllTargets(TestBoardCell cell, int length) {
+		for (TestBoardCell adjCell: cell.getAdjList(this)) {
+			if(visitedList.contains(adjCell)) {
+				break;
+			}
+			visitedList.add(adjCell);
+			if(length )
+		}
+	}
 	
 	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+		Set<TestBoardCell> targetsList = new HashSet<>();
+		visitedList.add(startCell);
+		findAllTargets(startCell,pathlength);
 		
 	}
 	
@@ -29,4 +43,5 @@ public class TestBoard {
 	public TestBoardCell getCell(int row, int col) {
 		return testCells[row][col];
 	}
+
 }

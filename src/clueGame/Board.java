f3e3f960e0395c76bd4a41ccdf9a823e48d8465d
mmccormick <mmccormick@mines.edu>
2021-10-26 20:29:03 -0600

    Removed hardcoding of closet character for adjacency detection

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -34,6 +34,8 @@ public class Board {
 	public static HashMap<Character, Room> rooms = new HashMap<Character, Room>();
 	// an array of special characters: <,>,^,*,#,v
 	public static ArrayList<String> specialC = new ArrayList<String>(Arrays.asList("<",">","^","v","#","*"));
+	
+	public static Character closetChar;
 
 
 	// Singleton object
@@ -69,76 +71,82 @@ public class Board {
 		initialRooms();
 		initialCells();
 		doorAdjList();
-		
-		
+
+
 	}
-		public void initialRooms() {
-			// create 11 rooms with constructors.
-			// store all the rooms in the map rooms.
-			// r is a single line in the setup file, split into words
-			for (ArrayList<String> r : setupStrings) {
-				// Looking for character that represents room in layout file
-				char c = r.get(2).charAt(0);
-				// Creating new room with name in constructor
-				Room room = new Room(r.get(1));
-				// Checking if room is of room with door or regular space
-				if (r.get(0).equals("Room")) {
-					room.setDoorRoom();
-				}
-				// Adding room to map with key of character that represents it in layout file
-				rooms.put(c, room);
+	public void initialRooms() {
+		// create 11 rooms with constructors.
+		// store all the rooms in the map rooms.
+		// r is a single line in the setup file, split into words
+		for (ArrayList<String> r : setupStrings) {
+			// Looking for character that represents room in layout file
+			char c = r.get(2).charAt(0);
+			// Creating new room with name in constructor
+			Room room = new Room(r.get(1));
+			
+			// Getting character that represents closet
+			if (room.getName().equals("Unused")) {
+				closetChar = r.get(2).charAt(0);
 			}
+			
+			// Checking if room is of room with door or regular space
+			if (r.get(0).equals("Room")) {
+				room.setDoorRoom();
+			}
+			// Adding room to map with key of character that represents it in layout file
+			rooms.put(c, room);
 		}
-		
-public void initialCells() {
-	// Running through layoutStrings 2d array list to create 2d array of cells based on entries
-			for (int r = 0; r < numRows; r++) {
-				for (int c = 0; c < numCols; c++) {
-					// Getting string that will be used to construct cell
-					String layoutString = layoutStrings.get(r).get(c);
-					// Creating new cell at specified row and column with its own string that represents it in the layout
-					grid[r][c] = new BoardCell(r, c, layoutString);
-					// Setting cell to be in room based on first char in layout board
-					if (this.getRoom(layoutString.charAt(0)).getDoorRoom()) {
-						grid[r][c].setIsRoom(true);
-					}
-					// Checking if there is a special character after room character to set attributes of cell
-					if (layoutString.length() > 1) {
-						// Getting room object using first character in layoutString
-						Room room = this.getRoom(layoutString.charAt(0));
-						// Changing things about cell depending on what second char is
-						char secondChar = layoutString.charAt(1);
-						if (secondChar == '#') {
-							room.setLabelCell(grid[r][c]);
-							// set the as the Center cell of the room 
-						} else if (secondChar == '*') {
-							room.setCenterCell(grid[r][c]);
-							// set cell of door way to be true. 
-						} else if (secondChar == '<' || secondChar == '>' || secondChar == '^' || secondChar == 'v') {
-							grid[r][c].setDoorway();
-							// set the cell as the secret path for the room.
-						} else {
-							room.setSecretPath(grid[r][c]);
-						}
-					}
+	}
+
+	public void initialCells() {
+		// Running through layoutStrings 2d array list to create 2d array of cells based on entries
+		for (int r = 0; r < numRows; r++) {
+			for (int c = 0; c < numCols; c++) {
+				// Getting string that will be used to construct cell
+				String layoutString = layoutStrings.get(r).get(c);
+				// Creating new cell at specified row and column with its own string that represents it in the layout
+				grid[r][c] = new BoardCell(r, c, layoutString);
+				// Setting cell to be in room based on first char in layout board
+				if (this.getRoom(layoutString.charAt(0)).getDoorRoom()) {
+					grid[r][c].setIsRoom(true);
 				}
-			}
-
-}
-// get the adjacent list for rooms by getting the adjacent list for the doors.
-		public void doorAdjList() {
-			for (BoardCell[] r : grid) {
-				for (BoardCell c : r) {
-					if (c.isDoorway()) {
-						theInstance.getAdjList(c);
+				// Checking if there is a special character after room character to set attributes of cell
+				if (layoutString.length() > 1) {
+					// Getting room object using first character in layoutString
+					Room room = this.getRoom(layoutString.charAt(0));
+					// Changing things about cell depending on what second char is
+					char secondChar = layoutString.charAt(1);
+					if (secondChar == '#') {
+						room.setLabelCell(grid[r][c]);
+						// set the as the Center cell of the room 
+					} else if (secondChar == '*') {
+						room.setCenterCell(grid[r][c]);
+						// set cell of door way to be true. 
+					} else if (secondChar == '<' || secondChar == '>' || secondChar == '^' || secondChar == 'v') {
+						grid[r][c].setDoorway();
+						// set the cell as the secret path for the room.
+					} else {
+						room.setSecretPath(grid[r][c]);
 					}
-
 				}
 			}
 		}
-	
-		
-	
+
+	}
+	// get the adjacent list for rooms by getting the adjacent list for the doors.
+	public void doorAdjList() {
+		for (BoardCell[] r : grid) {
+			for (BoardCell c : r) {
+				if (c.isDoorway()) {
+					theInstance.getAdjList(c);
+				}
+
+			}
+		}
+	}
+
+
+
 
 	// Pathfinding methods
 
@@ -270,14 +278,14 @@ public void initialCells() {
 		if (!boardCell.inRoom) {
 			boardCell.adjacent.clear();
 		}
-		
+
 		BoardCell[] orthoCells = getValidCells(boardCell);
 
 		for (int n = 0; n < orthoCells.length; n++) {
 			// Logic for doorways, only adds room to adjacency list if door is facing that room
 
 			if (boardCell.isDoorway) { 
-				
+
 				DoorDirection rightDir = DoorDirection.UP;
 				if (n == 1) {
 					rightDir = DoorDirection.DOWN;
@@ -297,13 +305,13 @@ public void initialCells() {
 						boardCell.adjacent.add(orthoCells[n]);
 					}
 				}
-				
+
 				// Logic for regular pathways
 			} else if (!boardCell.inRoom){
 				if (orthoCells[n] != null && !orthoCells[n].inRoom) {
 					boardCell.adjacent.add(orthoCells[n]);
 				}
-			// check the cell in the room.
+				// check the cell in the room.
 			} else if (boardCell.inRoom) {
 				Room room = getRoom(boardCell.layoutString.charAt(0));
 				if(room.getSecretPath()) {
@@ -314,14 +322,14 @@ public void initialCells() {
 				}
 			}
 		}
-		
+
 		return boardCell.adjacent;
 	}
-	
+
 	public BoardCell[] getValidCells(BoardCell boardCell) {
-		
+
 		BoardCell[] orthoCells = {null, null, null, null};
-		
+
 		// Checking if directions are valid based on if they are on edge of the board or are in closet
 		if (boardCell.row > 0) {
 			orthoCells[0] = getCell(boardCell.row - 1, boardCell.col);
@@ -339,17 +347,17 @@ public void initialCells() {
 			orthoCells[3] = getCell(boardCell.row, boardCell.col + 1);
 			orthoCells[3] = checkTileValidity(orthoCells[3]);
 		}
-		
+
 		return orthoCells;
 	}
-	
+
 	private BoardCell checkTileValidity(BoardCell boardCell) {
-		if (boardCell.getLayoutString().charAt(0) == 'X' || boardCell.getOccupied()) {
+		if (boardCell.getLayoutString().charAt(0) == closetChar || boardCell.getOccupied()) {
 			boardCell = null;
 		}
 		return boardCell;
 	}
-	
+
 	public static int getNumRows() {
 		return Board.numRows;
 	}

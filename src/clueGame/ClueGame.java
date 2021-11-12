e019cb65437cb42b4df31be7bea2180e6cce230e
Zhen Liu <zliu1@mines.edu>
2021-11-12 16:26:33 -0700

    turn the 3 panels into one frame and add blank Jtextfield for side panel

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,9 +1,14 @@
 package clueGame;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ClueGame extends JFrame {
 	public ClueGame() {
+
+
 		
 	}
 	
@@ -12,11 +17,29 @@ public class ClueGame extends JFrame {
 		Board board = Board.getInstance();
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
 		board.initialize();
+			
+		JFrame frame = new JFrame();
+		ClueGame center	= new ClueGame();  
+		GameControlPanel panel = new GameControlPanel(); 
+		GameSidePanel side= new GameSidePanel();
 		
-		ClueGame frame = new ClueGame();  // create the frame 
-		frame.setContentPane(Board.getInstance()); // put the panel in the frame
-		frame.setSize(1000, 700);  // size the frame
+		frame.setSize(800, 700);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
+
+		frame.setLayout(new BorderLayout());
+		frame.add(board,BorderLayout.CENTER);
+		
+	
+		frame.add(panel,BorderLayout.SOUTH);
+		
+	
+		frame.add(side,BorderLayout.EAST);
+		
+		side.setSize(frame.WIDTH-board.WIDTH,board.HEIGHT);
+		
+
+		
+		
 	}
 }
